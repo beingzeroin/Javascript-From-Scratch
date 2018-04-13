03bc065f008e727a14c660d65ede745f77f015e0
Sandeep <Sandeep>
2018-04-13 20:03:38 +0530

    Switch Case

---
+++ 010-switch-case.js
@@ -0,0 +1,12 @@
+var t = "alpha";
+
+switch(t){
+    case "alpha":
+        console.log("this is alpha");
+        break;
+    case "beta":
+        console.log("this is beta");
+        break;
+    default:
+        console.log("this is default");
+}
