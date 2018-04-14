e3d09b9e94d65271b7e91c15045927a21a069e29
Sandeep <Sandeep>
2018-04-14 08:15:04 +0530

    NodeJS NPM files

---
+++ NodeJS/001-consuming-modules.js
@@ -0,0 +1,4 @@
+const mod = require('./001-writing-modules');
+
+console.log(mod.cn);
+console.log(mod.add(3, 4));
---
+++ NodeJS/001-writing-modules.js
@@ -0,0 +1,8 @@
+var addFunction = function(a, b){
+    return a+b;
+}
+
+const companyName = "Being Zero Pvt Ltd";
+
+module.exports.add = addFunction;
+module.exports.cn = companyName;
